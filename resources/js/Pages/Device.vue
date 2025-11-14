0c97d0ec53ae534746035854bff7190f6e19a879
Horacio Alanis <horacioalanis@iMac-de-Horacio.local>
2025-11-14 10:32:49 -0600

    🚀 Deploy automático 2025-11-14 10:32:49

--- resources/js/Pages/Device.vue
+++ resources/js/Pages/Device.vue
@@ -272,6 +272,7 @@ socket.on('disconnect', (reason) => {
 socket.on('drop', (drop) => { 
   console.log(deviceImei.value)
   console.log(drop.imei)
+  console.log(drop)
   if (drop.imei == deviceImei.value) {
   console.log('llego el bueno', drop)
     lastdrop.value = {
